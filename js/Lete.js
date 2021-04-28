3414d7ee0d934bb8354678f1c34670d84b0df403
zhangc233 <1498262016@qq.com>
2021-04-28 13:45:14 +0800

    Site updated: 2021-04-28 13:45:14

--- js/Lete.js
+++ js/Lete.js
@@ -1,16 +1,16 @@
-/*添加图片top*/
-var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
-/*添加到返回顶部按钮下*/
-document.getElementById("go-up").innerHTML += top_up;
-
-/* 手机客户端导航栏默认隐藏 */
-var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
-var menus_item_child = mobile_sidebar_menus.getElementsByClassName("menus_item_child");
-var menus_expand = mobile_sidebar_menus.getElementsByClassName("expand");
-for (var i = 0; i < menus_item_child.length; i++) {
-    menus_item_child[i].style.display = "none";
-    menus_expand[i].className += " closed";
-}
+// /*添加图片top*/
+// var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
+// /*添加到返回顶部按钮下*/
+// document.getElementById("go-up").innerHTML += top_up;
+//
+// /* 手机客户端导航栏默认隐藏 */
+// var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
+// var menus_item_child = mobile_sidebar_menus.getElementsByClassName("menus_item_child");
+// var menus_expand = mobile_sidebar_menus.getElementsByClassName("expand");
+// for (var i = 0; i < menus_item_child.length; i++) {
+//     menus_item_child[i].style.display = "none";
+//     menus_expand[i].className += " closed";
+// }
 
 // 可爱的Title
 var OriginTitle = document.title;
@@ -78,39 +78,39 @@ function qipao() {
 qipao();
 
 
-$(document).ready(function(e) {
-    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Lete乐特');
-});
-
-var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
-var menus_item_child = mobile_sidebar_menus.getElementsByClassName(
-    "menus_item_child"
-);
-var menus_expand = mobile_sidebar_menus.getElementsByClassName("menus-expand");
-for (var i = 0; i < menus_item_child.length; i++) {
-    menus_item_child[i].style.display = "none";
-    menus_expand[i].className += " menus-closed";
-}
-
-var card_category_list = document.getElementsByClassName(
-    "card-category-list child"
-);
-var item = document.getElementsByClassName("card-category-list-item");
-function toggle(t) {
-    var display = t.parentElement.nextSibling.style.display;
-    if (display == "none") {
-        t.parentElement.nextSibling.style.display = "block";
-        t.parentElement.nextSibling.style.height = "100%";
-        t.className = t.className.replace("fa-chevron-up", "fa-chevron-down");
-    } else {
-        t.parentElement.nextSibling.style.display = "none";
-        t.className = t.className.replace("fa-chevron-down", "fa-chevron-up");
-    }
-}
-
-for (var i = 0; i < card_category_list.length; i++) {
-    card_category_list[i].style.display = "none";
-    card_category_list[i].style.transition = "all 1s";
-    card_category_list[i].previousSibling.innerHTML +=
-        '<i class="fa fa-chevron-up menus-expand  menus-closed" aria-hidden="true" style="margin-left:20px;" οnclick="toggle(this)"></i>';
-}
+// $(document).ready(function(e) {
+//     $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Lete乐特');
+// });
+//
+// var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
+// var menus_item_child = mobile_sidebar_menus.getElementsByClassName(
+//     "menus_item_child"
+// );
+// var menus_expand = mobile_sidebar_menus.getElementsByClassName("menus-expand");
+// for (var i = 0; i < menus_item_child.length; i++) {
+//     menus_item_child[i].style.display = "none";
+//     menus_expand[i].className += " menus-closed";
+// }
+//
+// var card_category_list = document.getElementsByClassName(
+//     "card-category-list child"
+// );
+// var item = document.getElementsByClassName("card-category-list-item");
+// function toggle(t) {
+//     var display = t.parentElement.nextSibling.style.display;
+//     if (display == "none") {
+//         t.parentElement.nextSibling.style.display = "block";
+//         t.parentElement.nextSibling.style.height = "100%";
+//         t.className = t.className.replace("fa-chevron-up", "fa-chevron-down");
+//     } else {
+//         t.parentElement.nextSibling.style.display = "none";
+//         t.className = t.className.replace("fa-chevron-down", "fa-chevron-up");
+//     }
+// }
+//
+// for (var i = 0; i < card_category_list.length; i++) {
+//     card_category_list[i].style.display = "none";
+//     card_category_list[i].style.transition = "all 1s";
+//     card_category_list[i].previousSibling.innerHTML +=
+//         '<i class="fa fa-chevron-up menus-expand  menus-closed" aria-hidden="true" style="margin-left:20px;" οnclick="toggle(this)"></i>';
+// }
