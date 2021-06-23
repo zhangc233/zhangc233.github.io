cccc81580441bf7ef2dfad5dfb1b30517042bf6c
zhangc233 <1498262016@qq.com>
2021-06-23 22:44:57 +0800

    Site updated: 2021-06-23 22:44:55

--- js/back.js
+++ js/back.js
@@ -1,9 +1,10 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var backimg =[
     "url(https://cdn.jsdelivr.net/gh/zhangc233/pic@master/img/%E5%8A%A8%E6%BC%AB5.jpeg)",
-    "url(https://cdn.jsdelivr.net/gh/zhangc233/pic@master/img/%E5%8A%A8%E6%BC%AB3.jpeg)",
+    "url(https://cdn.jsdelivr.net/gh/zhangc233/pic@master/img/%E5%A3%81%E7%BA%B812.jpeg)",
     "url(https://cdn.jsdelivr.net/gh/zhangc233/pic@master/img/zcbase.jpg)",
-    "url(https://cdn.jsdelivr.net/gh/zhangc233/pic@master/img/%E5%8A%A8%E6%BC%AB1.jpeg)"
+    "url(https://cdn.jsdelivr.net/gh/zhangc233/pic@master/img/%E5%8A%A8%E6%BC%AB1.jpeg)",
+	"url(https://cdn.jsdelivr.net/gh/zhangc233/pic@master/img/%E5%8A%A8%E6%BC%AB5.jpeg)"
 ];
 //获取背景图片总数，生成随机数
 var bgindex =Math.ceil(Math.random() * (backimg.length-1));
